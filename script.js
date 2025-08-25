041281d684ba44a8e8d4b07146098463c5f2d9fa
Anandrajoriya <anandrajoriya03@gmail.com>
2025-08-25 08:38:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// A little greeting for fellow developers checking out the console!
+console.log("Thanks for checking out my resume! Feel free to reach out.");
+
+// You can add more interactivity here in the future,
+// such as a theme switcher (light/dark mode) or filterable projects.
